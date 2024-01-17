e0c3a41497d4e9e8041971fb69fd820024092783
Dailan Bueno <daylansantos@gmail.com>
2024-01-17 10:43:33 -0300

    adicionando spring security

--- src/main/java/io/github/daylanbueno/authapi/controllers/UsuarioController.java
+++ src/main/java/io/github/daylanbueno/authapi/controllers/UsuarioController.java
@@ -3,10 +3,7 @@ package io.github.daylanbueno.authapi.controllers;
 import io.github.daylanbueno.authapi.dtos.UsuarioDto;
 import io.github.daylanbueno.authapi.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -18,5 +15,10 @@ public class UsuarioController {
     @PostMapping
     private UsuarioDto salvar(@RequestBody UsuarioDto usuarioDto) {
         return  usuarioService.salvar(usuarioDto) ;
+    }
+
+    @GetMapping
+    private String getOk() {
+        return "Ok";
     }
 }

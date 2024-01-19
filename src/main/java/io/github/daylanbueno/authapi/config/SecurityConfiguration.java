7f2ae0c34bdd8165bc9b7c93f7af96f3f788006b
Dailan Bueno <daylansantos@gmail.com>
2024-01-19 17:39:58 -0300

    criando filter chain

---
+++ src/main/java/io/github/daylanbueno/authapi/config/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package io.github.daylanbueno.authapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authoriza ->
+                        authoriza.requestMatchers(HttpMethod.GET, "/usuarios").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                ).build();
+    }
+
+}
